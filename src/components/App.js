4153112e22fcc2f22ac25d08c1112f38d8626010
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-11-19 11:44:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <h1 data-ns-test="project-name">Interactive Portfolio</h1>
+      <p data-ns-test="project-description">
+        A dynamic portfolio showcasing my skills and projects with interactive features.
+      </p>
+    </div>
   )
 }
 
